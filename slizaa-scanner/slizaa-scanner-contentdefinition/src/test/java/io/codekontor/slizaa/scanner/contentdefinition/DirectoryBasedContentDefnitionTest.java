97cf3b3572e0ed844da9598a350b3a54c4108975
Gerd Wuetherich <gerd.wuetherich@code-kontor.io>
2019-09-10 22:34:21 +0200

    fixed test setup

--- slizaa-scanner/slizaa-scanner-contentdefinition/src/test/java/io/codekontor/slizaa/scanner/contentdefinition/DirectoryBasedContentDefnitionTest.java
+++ slizaa-scanner/slizaa-scanner-contentdefinition/src/test/java/io/codekontor/slizaa/scanner/contentdefinition/DirectoryBasedContentDefnitionTest.java
@@ -17,70 +17,96 @@
  */
 package io.codekontor.slizaa.scanner.contentdefinition;
 
+import io.codekontor.mvnresolver.MvnResolverServiceFactoryFactory;
+import io.codekontor.mvnresolver.api.IMvnResolverService;
 import io.codekontor.slizaa.scanner.spi.contentdefinition.IContentDefinition;
 import io.codekontor.slizaa.scanner.spi.contentdefinition.IContentDefinitionProviderFactory;
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DirectoryBasedContentDefnitionTest {
 
-    private IContentDefinitionProviderFactory<DirectoryBasedContentDefinitionProvider> _providerFactory;
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
 
-    @Before
-    public void setup() {
-        _providerFactory = new DirectoryBasedContentDefinitionProviderFactory();
-    }
+    private IContentDefinitionProviderFactory<DirectoryBasedContentDefinitionProvider> _providerFactory  = new DirectoryBasedContentDefinitionProviderFactory();
 
     @Test
-    @Ignore
-    public void testDirectoryBasedContentDefnition_1() {
+    public void testDirectoryBasedContentDefinition() throws IOException {
 
-        // TODO TEST-SETUP
+        //
+        File testFolder =
+                setupTestFolder("org.springframework:spring-core:5.1.9.RELEASE", "org.springframework:spring-beans:5.1.9.RELEASE");
 
+        //
         DirectoryBasedContentDefinitionProvider contentDefinitionProvider = _providerFactory.emptyContentDefinitionProvider();
-        contentDefinitionProvider.add(new File("c:\\tmp\\sl"));
-
+        contentDefinitionProvider.add(testFolder);
         List<IContentDefinition> contentDefinitions = contentDefinitionProvider.getContentDefinitions();
 
+        // assert the result
         assertThat(contentDefinitions).hasSize(2);
-        assertThat(contentDefinitions.get(0).getName()).isEqualTo("spring-core");
-        assertThat(contentDefinitions.get(1).getName()).isEqualTo("spring-beans");
+        assertThat(contentDefinitions.stream().map(cd -> cd.getName()).collect(Collectors.toList()))
+                .containsExactlyInAnyOrder("spring-core", "spring-beans");
+        assertThat(contentDefinitions.stream().map(cd -> cd.getVersion()).collect(Collectors.toList()))
+                .containsExactlyInAnyOrder("5.1.9.RELEASE", "5.1.9.RELEASE");
     }
 
-/*    @Test
-    public void testMvnBasedContentDefnition_2() {
+    /**
+     *
+     * @return
+     * @throws IOException
+     */
+    private File setupTestFolder(String... mavenCoordinates) throws IOException {
 
-        String externalRepresentation = "org.springframework:spring-core:5.1.6.RELEASE, org.springframework:spring-beans:5.1.6.RELEASE";
+        // create the IMvnResolverService
+        IMvnResolverService mvnResolverService = MvnResolverServiceFactoryFactory.createNewResolverServiceFactory().
+                newMvnResolverService().withMavenCentralRepo(true).create();
 
-        MvnBasedContentDefinitionProvider contentDefinitionProvider = _providerFactory.fromExternalRepresentation(externalRepresentation);
+        // create the test folder
+        File tempFolder = testFolder.newFolder("folder");
 
-        List<IContentDefinition> contentDefinitions = contentDefinitionProvider.getContentDefinitions();
+        // populate it
+        File[] files = mvnResolverService.resolve(false, mavenCoordinates);
+        for (File sourceFile : files) {
+            File targetFile = new File(tempFolder, sourceFile.getName());
+            copyFile(sourceFile, targetFile);
+        }
 
-        assertThat(contentDefinitions).hasSize(2);
-        assertThat(contentDefinitions.get(0).getName()).isEqualTo("spring-core");
-        assertThat(contentDefinitions.get(1).getName()).isEqualTo("spring-beans");
+        // return the result
+        return tempFolder;
     }
 
-    @Test
-    public void testMvnBasedContentDefnition_3() {
+    /**
+     *
+     * @param source
+     * @param dest
+     * @throws IOException
+     */
+    private static void copyFile(File source, File dest) throws IOException {
 
-        MvnBasedContentDefinitionProvider contentDefinitionProvider = _providerFactory.emptyContentDefinitionProvider();
-        contentDefinitionProvider.addArtifact("org.springframework:spring-core:5.1.6.RELEASE");
-        contentDefinitionProvider.addArtifact("org.springframework:spring-beans:5.1.6.RELEASE");
+        FileChannel sourceChannel = null;
+        FileChannel destChannel = null;
 
-        String externalRepresentation = _providerFactory.toExternalRepresentation(contentDefinitionProvider);
-
-        MvnBasedContentDefinitionProvider contentDefinitionProvider2 = _providerFactory.fromExternalRepresentation(externalRepresentation);
-        List<IContentDefinition> contentDefinitions = contentDefinitionProvider2.getContentDefinitions();
-
-        assertThat(contentDefinitions).hasSize(2);
-        assertThat(contentDefinitions.get(0).getName()).isEqualTo("spring-core");
-        assertThat(contentDefinitions.get(1).getName()).isEqualTo("spring-beans");
-    }*/
+        try {
+            sourceChannel = new FileInputStream(source).getChannel();
+            destChannel = new FileOutputStream(dest).getChannel();
+            destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+        } finally{
+            sourceChannel.close();
+            destChannel.close();
+        }
+    }
 }
