ea5853b4f644d9547f66b4752104bcecf7a33d30
Gerd Wuetherich <gerd.wuetherich@code-kontor.io>
2021-05-01 23:12:10 +0200

    Set setAllowCredentials to false in cors filter

--- slizaa-server/slizaa-server-main/src/main/java/io/codekontor/slizaa/server/SlizaaServerConfiguration.java
+++ slizaa-server/slizaa-server-main/src/main/java/io/codekontor/slizaa/server/SlizaaServerConfiguration.java
@@ -53,7 +53,7 @@ public class SlizaaServerConfiguration {
     public FilterRegistrationBean<CorsFilter> corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
